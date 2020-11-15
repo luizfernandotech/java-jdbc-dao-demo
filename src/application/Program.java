0f12ca72733c336b7aff660fcb12082b6badbf23
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-11-15 15:49:08 +0000

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,17 @@ public class Program {
 
     public static void main(String[] args) {
 
+        System.out.println("==== Exemple 1 - Department Manual Instance ====");
         Department dep = new Department(1, "Books");
         System.out.println(dep);
 
+        System.out.println("\n==== Exemple 2 - Seller Manual Instance ====");
         Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dep);
         System.out.println(seller);
 
+        System.out.println("\n==== Exemple 3 - Seller findById ====");
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
