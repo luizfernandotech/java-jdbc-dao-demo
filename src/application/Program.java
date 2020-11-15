58ebec7c0f9fdd58bc2885516d8ab5a95df8f81e
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-11-15 13:41:37 +0000

    Department entity class
    Entity class checklist:
    - Attributes
    - Constructors
    - Getters/Setters
    - hashCode and equals
    - toString
    - implements Serializable

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
-public class Program {
-    public static void main(String[] args) {
+import model.entities.Department;
 
+public class Program {
+
+    public static void main(String[] args) {
+        Department dep = new Department(1, "Books");
+        System.out.println(dep);
     }
 }
